06bb42cadc0d6aec2388e6b366fde8c7d83d56fc
Philippe Haerens <haerphi@gmail.com>
2019-10-01 13:04:36 +0200

    ajout de la méthode delete

--- utils.ts
+++ utils.ts
@@ -1,6 +1,17 @@
 import axios from "axios";
 
-export async function axiosGet(url: string){
-    return await axios
-        .get("https://character-database.becode.xyz/characters").then(rep => { return rep.data });
+export async function axiosGet(url: string) {
+  return await axios
+    .get(`https://character-database.becode.xyz/characters/${url}`)
+    .then(rep => {
+      return rep.data;
+    });
+}
+
+export async function axiosDelete(id: string) {
+  return await axios
+    .delete(`https://character-database.becode.xyz/characters/:${url}`)
+    .then(rep => {
+      return rep.data;
+    });
 }
