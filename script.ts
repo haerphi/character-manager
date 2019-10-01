725769b4c51f6ad136d5e5a0f10bda3827cf8dee
Philippe Haerens <haerphi@gmail.com>
2019-10-01 13:52:57 +0200

    modify link of img

--- script.ts
+++ script.ts
@@ -1,10 +1,10 @@
 import { axiosGet } from "./utils";
 
 function buildCards(table): void {
-
-    table.forEach(element => {
-        let temp: string = `<div class="card" width="100px" height="100px">
-            <img src="https://place-hold.it/100x100" alt="Image vide">
+  console.log(table);
+  table.forEach(element => {
+    let temp: string = `<div class="card" width="100px" height="100px">
+            <img src="data:image/gif;base64,${element.image}" alt="Image vide">
             <div class="card-body">
                 <h5 class="card-title text-center name">${element.name}</h5>
                 <p class="card-text text-center short-desc">${element.shortDescription}</p>
@@ -12,12 +12,14 @@ function buildCards(table): void {
                     <a href="#" class="btn btn-primary">Modify</a>
                     <a href="#" class="btn btn-primary">Delete</a>
                 </div>
+                <input type="hidden" class="charac-id" value="${element.id}" >
             </div>
         </div>`;
-        let target: HTMLElement = document.querySelector(".personnalcenter");
-        target.innerHTML += temp;
-    });
+    let target: HTMLElement = document.querySelector(".personnalcenter");
+    target.innerHTML += temp;
+  });
 }
 
-axiosGet("")
-    .then(rep => { buildCards(rep) });
+axiosGet("").then(rep => {
+  buildCards(rep);
+});
