9ad56a967cae612af306af590cb38e6932a2fd67
Philippe Haerens <haerphi@gmail.com>
2019-10-02 11:43:28 +0200

    modify Single page

--- single/single.ts
+++ single/single.ts
@@ -1,7 +1,31 @@
+import { axiosGet, axiosDelete } from "../utils";
+
 const url_string: string = window.location.href; //récupère l'url de la page sous forme de string
 const url: URL = new URL(url_string); //transforme le string en URL
 const id: string = url.searchParams.get("id"); //id de character
 
-//Requête axios
+axiosGet(id).then(rep => {
+  console.log(rep);
+  const name: HTMLElement = document.querySelector(".name");
+  name.textContent = rep.name;
 
-//afficher le résultat dans la page (dans le then de axios)
+  const img: HTMLImageElement = document.querySelector(
+    ".image"
+  ) as HTMLImageElement;
+  img.src = `data:image/gif;base64,${rep.image}`;
+
+  const shortdesc: HTMLElement = document.querySelector(".short-description");
+  shortdesc.textContent = rep.shortDescription;
+
+  const desc: HTMLElement = document.querySelector(".description");
+  desc.textContent = rep.description;
+});
+
+//BOUTON DELETE
+document.querySelector(".deleteBTN").addEventListener("click", () => {
+  if (confirm("Voulez-vous vraiment supprimer cette élement ?")) {
+    axiosDelete(id).then(() => {
+      window.location.href = "../";
+    });
+  }
+});
